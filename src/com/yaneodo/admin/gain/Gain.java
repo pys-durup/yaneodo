a63c3b6f87728df783fb2f9b2a90192fee76c490
jibongYun <yunjibong@naver.com>
2021-02-24 18:13:18 +0900

    수익 완료
    차트 수정, 수익 달력 완료

--- src/com/yaneodo/admin/gain/Gain.java
+++ src/com/yaneodo/admin/gain/Gain.java
@@ -1,6 +1,9 @@
 package com.yaneodo.admin.gain;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -20,15 +23,48 @@ public class Gain extends HttpServlet {
 		//2. DB작업하기 -> select
 		//3. 결과처리
 		
-		
+		req.setCharacterEncoding("UTF-8");
 		//1.
-		String date = req.getParameter("date");
+		String date = req.getParameter("date");  //가져오기
 		
+		
+		  req.setAttribute("pickdate", date);
+		 
+		  /* 
+		 * if (req.getParameter("flag").equals("pick")) { req.setAttribute("flag",
+		 * "pick"); } else { req.setAttribute("flag", "today"); }
+		 */
 		//2. 
-		GainDAO dao = new GainDAO();
-		GainDTO dto = dao.getgain(date); 
 		
-		req.setAttribute("dto", dto);
+		Calendar now = Calendar.getInstance();
+		Calendar cal = Calendar.getInstance();
+		
+		if (date == null) {
+			date = String.format("%tF", now);
+		} else {
+			
+			try {
+				
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+				Date dates = formatter.parse(date);
+				cal.setTime(dates);
+				
+				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		
+		
+		GainDAO dao = new GainDAO();
+		GainDTO dtoday = dao.getgainday(date); 
+		GainDTO dtoweek = dao.getgainweek(date); 
+		GainDTO dtomonth = dao.getgainmonth(date); 
+		
+		
+		req.setAttribute("dto", dtoday);
+		req.setAttribute("dtoweek", dtoweek);
+		req.setAttribute("dtomonth", dtomonth);
 		
 		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/admin/gain/gain.jsp");
--- src/com/yaneodo/admin/gain/GainDAO.java
+++ src/com/yaneodo/admin/gain/GainDAO.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import com.yaneodo.DBUtil2;
 
@@ -28,10 +31,10 @@ public class GainDAO {
 		}
 	}
 
-	public GainDTO getgain(String date) {
+	public GainDTO getgainday(String date) {
 
-		String days = "21/02/02";
-		//String days = date.substring(0,2) +"/"+ date.substring(3,5)+"/"+date.substring(6,8) ;
+		//String days = "2021/02/02";
+		String days = date.substring(2,4) +"/"+ date.substring(5,7)+"/"+date.substring(8);
 		
 		try {
 			
@@ -42,6 +45,104 @@ public class GainDAO {
 			
 			rs = pstat.executeQuery();
 			
+			
+		
+			
+			GainDTO dto = new GainDTO();
+
+			if(rs.next()) {
+			
+				dto.setA(rs.getString("Atype"));
+				dto.setB(rs.getString("Btype"));
+				dto.setC(rs.getString("Ctype"));
+				dto.setD(rs.getString("Dtype"));
+				dto.setE(rs.getString("Etype"));
+						
+			}
+			
+			return dto;
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+
+
+	public GainDTO getgainweek(String date) {
+		
+		Calendar cal = Calendar.getInstance();
+		
+		String daystart = "";
+		String dayend = date.substring(2,4) +"/"+ date.substring(5,7)+"/"+date.substring(8);
+		
+		try {
+			
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+			Date dates = formatter.parse(date);
+			cal.setTime(dates);
+			cal.add(Calendar.DATE, -7);
+			
+			String end = String.format("%tF", cal);
+			
+			daystart = end.substring(2,4) +"/"+ end.substring(5,7)+"/"+end.substring(8);
+			
+			String sql = "select Atype, Btype, Ctype, Dtype, Etype, (Atype *100000+Btype*200000+Ctype*300000+Dtype*400000+Etype*500000) as sum from (select count(case when sp.type = '1' then 1 end) as Atype, count(case when sp.type = 'b상품' then 1 end) as Btype, count(case when sp.type = 'c상품' then 1 end) as Ctype, count(case when sp.type = 'd상품' then 1 end) as Dtype, count(case when sp.type = 'e상품' then 1 end) as Etype from tblServiceproduct sp inner join tblPayment pm on sp.serviceProductSeq = pm.serviceProductSeq where to_char(pm.paydate,'yy/mm/dd') between ? and ?)";
+			
+			pstat = conn.prepareStatement(sql);
+			pstat.setString(1, daystart);
+			pstat.setString(2, dayend);
+			
+			rs = pstat.executeQuery();
+			
+						
+			GainDTO dto = new GainDTO();
+
+			if(rs.next()) {
+			
+				dto.setA(rs.getString("Atype"));
+				dto.setB(rs.getString("Btype"));
+				dto.setC(rs.getString("Ctype"));
+				dto.setD(rs.getString("Dtype"));
+				dto.setE(rs.getString("Etype"));
+						
+			}
+			
+			return dto;
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+
+
+	public GainDTO getgainmonth(String date) {
+Calendar cal = Calendar.getInstance();
+		
+		String daystart = "";
+		String dayend = date.substring(2,4) +"/"+ date.substring(5,7)+"/"+date.substring(8);
+		
+		try {
+			
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+			Date dates = formatter.parse(date);
+			cal.setTime(dates);
+			cal.add(Calendar.DATE, -30);
+			
+			String end = String.format("%tF", cal);
+			
+			daystart = end.substring(2,4) +"/"+ end.substring(5,7)+"/"+end.substring(8);
+			
+			String sql = "select Atype, Btype, Ctype, Dtype, Etype, (Atype *100000+Btype*200000+Ctype*300000+Dtype*400000+Etype*500000) as sum from (select count(case when sp.type = '1' then 1 end) as Atype, count(case when sp.type = 'b상품' then 1 end) as Btype, count(case when sp.type = 'c상품' then 1 end) as Ctype, count(case when sp.type = 'd상품' then 1 end) as Dtype, count(case when sp.type = 'e상품' then 1 end) as Etype from tblServiceproduct sp inner join tblPayment pm on sp.serviceProductSeq = pm.serviceProductSeq where to_char(pm.paydate,'yy/mm/dd') between ? and ?)";
+			
+			pstat = conn.prepareStatement(sql);
+			pstat.setString(1, daystart);
+			pstat.setString(2, dayend);
+			
+			rs = pstat.executeQuery();
+			
+						
 			GainDTO dto = new GainDTO();
 
 			if(rs.next()) {
