4b5e29a092ccaced05497a92c0b81aa8d766d6e3
durup <enenfnq@gmail.com>
2021-02-27 15:41:20 +0900

    Add match/list pagination
    매치업 프로필 리스트 페이징 추가

--- src/com/yaneodo/company/match/List.java
+++ src/com/yaneodo/company/match/List.java
@@ -96,6 +96,7 @@ public class List extends HttpServlet {
 		//2. 
 		MatchDAO dao = new MatchDAO();
 		
+		// 페이징 - 페이지 수 계산
 		totalCount = dao.getTotalCount(map); // 총 게시물 수
 		System.out.println("게시물 수 : " + totalCount);
 		
@@ -104,6 +105,72 @@ public class List extends HttpServlet {
 		
 		String pagebar = "";
 		
+		
+		loop = 1;
+		//n = 1;
+		n = ((nowPage - 1) / blockSize) * blockSize + 1;
+		
+		// 상태 유지를 위한 코드
+		// search jobtype isdibs isread ismatch
+		if(jobtype == null) jobtype = "";
+		if(isread == null) isread = "";
+		if(search == null) search = "";
+		if(isdibs == null) isdibs = "";
+		if(ismatch == null) ismatch = "";
+		
+		
+		// 페이지 바 코드 동적으로 생성
+		
+		// 이전 페이지로 이동
+		if (n == 1) {
+			pagebar += String.format("<li class='disabled'>"
+					+ "	            <a href=\"#!\" aria-label=\"Previous\">"
+					+ "	                <span aria-hidden=\"true\">&laquo;</span>"
+					+ "	            </a>"
+					+ "	        </li>");
+			
+		} else {
+			pagebar += String.format("<li>"
+					+ "	            <a href=\"/yaneodo/company/match/list.do?page=%d&jobtype="+jobtype+"&isread="+isread+"&isdibs="+isdibs+"&ismatch="+ismatch+"&search="+search+"\" aria-label=\"Previous\">"
+					+ "	                <span aria-hidden=\"true\">&laquo;</span>"
+					+ "	            </a>"
+					+ "	        </li>", n-1);			
+		}
+		
+		
+		
+		
+		// 페이지 번호
+		while (!(loop > blockSize || n > totalPage)) {
+			
+			if (nowPage == n) {
+				pagebar += "<li class='active'>";				
+			} else {
+				pagebar += "<li>";
+			}
+			pagebar += String.format("<a href=\"/yaneodo/company/match/list.do?page=%d&jobtype="+jobtype+"&isread="+isread+"&isdibs="+isdibs+"&ismatch="+ismatch+"&search="+search+"\">%d</a></li> ",n, n);
+			
+			loop++;
+			n++;
+			
+		}
+		
+		
+		// 다음 10페이지로 이동
+		if (n > totalPage) {
+			pagebar += String.format("<li class='disabled'>"
+					+ "	            <a href=\"#!\" aria-label=\"Next\">"
+					+ "	                <span aria-hidden=\"true\">&raquo;</span>"
+					+ "	            </a>"
+					+ "	        </li>");
+			
+		} else {
+			pagebar += String.format("<li>"
+					+ "	            <a href=\"/yaneodo/company/match/list.do?page=%d&jobtype="+jobtype+"&isread="+isread+"&isdibs="+isdibs+"&ismatch="+ismatch+"&search="+search+"\" aria-label=\"Next\">"
+					+ "	                <span aria-hidden=\"true\">&raquo;</span>"
+					+ "	            </a>"
+					+ "	        </li>", n);			
+		}
 		
 		
 		
@@ -124,6 +191,7 @@ public class List extends HttpServlet {
 		req.setAttribute("isread", isread);
 		req.setAttribute("isdibs", isdibs);
 		req.setAttribute("ismatch", ismatch);
+		req.setAttribute("pagebar", pagebar);
 		
 		
 		
--- src/com/yaneodo/company/match/MatchDAO.java
+++ src/com/yaneodo/company/match/MatchDAO.java
@@ -44,8 +44,9 @@ public class MatchDAO {
 			
 			String where = "";
 			int count = map.size();
+			System.out.println("listProfile map.size() : " + map.size());
 		
-			if (map.size() > 1 ) {
+			if (map.size() > 3) {
 				where = "where ";
 			}
 			
@@ -109,10 +110,10 @@ public class MatchDAO {
 				}	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 			}
 			
-
+			//페이징 이전 쿼리
+			//String sql = String.format("select * from vwMatchList %s", where);
 			
-			
-			String sql = String.format("select * from vwMatchList %s", where);
+			String sql = String.format("select * from (select a.*, rownum as rnum from (select * from vwMatchList %s order by profileseq desc) a) where rnum between %s and %s", where, map.get("begin"), map.get("end"));
 			
 			System.out.println(sql);
 			pstat = conn.prepareStatement(sql);
@@ -169,5 +170,99 @@ public class MatchDAO {
 			System.out.println(e);
 		}
 		return null;
+	}
+
+	/**
+	 * match/list 서블릿에서 프로필의 총 개수를 리턴
+	 * @return
+	 */
+	public int getTotalCount(HashMap<String, String> map) {
+		
+		try {
+			
+			String where = "";
+			int count = map.size();
+			System.out.println("getTotalCount map.size() : " + map.size());
+		
+			if (map.size() > 3) {
+				where = "where ";
+			}
+			
+			if (map.get("search") != null) {
+				// 검색어가 있을때 > 검색중일때
+				where += String.format("(jobtype like '%%%s%%' or company like '%%%s%%' or major like '%%%s%%' or school like '%%%s%%')", map.get("search"), map.get("search"), map.get("search"), map.get("search") );
+				count--;
+			}
+			
+			if (map.get("jobtype") != null) {
+				// 직무가 선택되었을때 
+				if (count == map.size()) {
+					where += String.format(" jobtype = '%s'", map.get("jobtype"));
+					count--;
+				} else {
+					where += String.format(" and jobtype = '%s'", map.get("jobtype"));
+				}	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+			}
+			
+			if (map.get("isdibs") != null) {
+				// 찜목록 선택되었을때 
+				if (count == map.size()) {
+					where += String.format(" profileseq in (select profileseq from tbldibs where companymemberseq = '%s')", map.get("session"));
+					count--;
+				} else {
+					where += String.format(" and profileseq in (select profileseq from tbldibs where companymemberseq = '%s')", map.get("session"));
+				}	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+			}
+			
+			
+			
+			if (map.get("isread") != null) {
+				
+				if (map.get("isread").equals("1")) {
+					// 열람한목록 선택되었을때 
+					if (count == map.size()) {
+						where += String.format(" profileseq in (select profileseq from tblresumeread where companymemberseq = '%s')", map.get("session"));
+						count--;
+					} else {
+						where += String.format(" and profileseq in (select profileseq from tblresumeread where companymemberseq = '%s')", map.get("session"));
+					}	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+				} else {
+					// 미열람목록 선택되었을때
+					if (count == map.size()) {
+						where += String.format(" profileseq not in (select profileseq from tblresumeread where companymemberseq = '%s')", map.get("session"));
+						count--;
+					} else {
+						where += String.format(" and profileseq not in (select profileseq from tblresumeread where companymemberseq = '%s')", map.get("session"));
+					}	        					
+				}
+			}
+			
+			
+			if (map.get("ismatch") != null) {
+				// 면접제안 목록
+				if (count == map.size()) {
+					where += String.format(" profileseq in (select profileseq from tblresumeread rr inner join tblmatchup m on rr.resumereadseq = m.resumereadseq where rr.companymemberseq = %s)", map.get("session"));
+					count--;
+				} else {
+					where += String.format(" and profileseq in (select profileseq from tblresumeread rr inner join tblmatchup m on rr.resumereadseq = m.resumereadseq where rr.companymemberseq = %s)", map.get("session"));
+				}	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+			}
+			
+			String sql = String.format("select count(*) as cnt from vwMatchList %s", where);
+			
+			System.out.println(sql);
+			
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+			
+			if (rs.next()) {
+				return rs.getInt("cnt");
+			}
+			
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return 0;
 	}
 }
