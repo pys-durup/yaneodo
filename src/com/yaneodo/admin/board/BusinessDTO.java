88d8cc7ad548bcb212039811fd3b0d3154286112
abcd4501 <69293712+abcd4501@users.noreply.github.com>
2021-03-01 06:52:03 +0900

    회사리스트 & 회사정보 추가
    memberList
    member
    추가

--- src/com/yaneodo/admin/board/BusinessDTO.java
+++
@@ -1,5 +0,0 @@
-package com.yaneodo.admin.board;
-
-public class BusinessDTO {
-
-}
---
+++ src/com/yaneodo/admin/board/Test.java
@@ -0,0 +1,17 @@
+package com.yaneodo.admin.board;
+
+import java.util.Calendar;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Calendar c = Calendar.getInstance();
+		
+		System.out.println(c.get(Calendar.YEAR));
+		
+		
+		
+	}
+
+}
